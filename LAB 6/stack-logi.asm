02fb079ca7ec079fc10c3ea21e07326e73aa5f64
Nosratee Jahan Naba <187439614+soloflare1@users.noreply.github.com>
2026-05-11 13:36:46 +0600

    Create stack-logi.asm

---
+++ LAB 6/stack-logi.asm
@@ -0,0 +1,30 @@
+   
+                                                                                                                                                
+;MOV  AX, 1234H
+;MOV  BX, 0F45H
+;PUSH AX
+;PUSH BX   
+;POP  CX       
+ 
+ 
+;MOV AX, 45A8H
+;MOV CX, 0E87H
+;AND CX, AX 
+
+
+ 
+MOV AX, 45A8H
+MOV CX, 0E87H
+OR CX, AX   
+
+ 
+MOV AX, 45A8H
+MOV CX, 0E87H
+XOR CX, AX   
+
+
+ 
+
+MOV CX, 0E87H
+NOT CX   
+                                    
